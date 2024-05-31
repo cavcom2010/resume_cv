2d3942bfccf0104f3ac30cac125ba813a8981a54
cavcom2010 <email@email.com>
2024-05-31 07:22:23 +0100

    added the reset button in the contact form

--- resume_cv/forms.py
+++ resume_cv/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class ContactForm(forms.Form):
     full_name = forms.CharField(max_length=250, required=True)
-    email = forms.EmailField(max_length=250, required=True, widget=forms.TextInput(attrs={'class': 'form-group d-flex flex-column-reverse'}))
-    message = forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'form-control', 'placeholder': 'Message required*',
-                                                                          'rows':5, 'id':'cf-message'})
+    email = forms.EmailField(max_length=250, required=True, widget=forms.TextInput())
+    message = forms.CharField(max_length=500, required=True, widget=forms.Textarea(attrs={'placeholder': 'Message required*',
+                                                                          'rows':10})
                                                                           )
